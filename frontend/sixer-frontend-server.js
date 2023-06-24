7b05d35f0c105e279f6b3103803b15517734be55
Komla Afoutou <komlaafoutou@microsoft.com>
2023-06-24 16:19:22 -0700

    .

--- frontend/sixer-frontend-server.js
+++ frontend/sixer-frontend-server.js
@@ -31,30 +31,6 @@ app.get('/events', (req, res) => {
   res.sendFile(path.join(__dirname + '/events.html'));
 });
 
-app.post('/customer-preferences', (req, res) => {
-    console.log(req.body)
-    // res.send(req.body);
-    https.post('sixinthecity.ai/api/survey', res => {
-      let data = [];
-      const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-      console.log('Status Code:', res.statusCode);
-      console.log('Date in Response header:', headerDate);
-
-      res.on('data', chunk => {
-        data.push(chunk);
-      });
-
-      res.on('end', () => {
-        console.log('Response ended: ');
-        const output = JSON.parse(Buffer.concat(data).toString());
-        console.log(output);
-        res.send(output)
-      });
-  }).on('error', err => {
-    console.log('Error: ', err.message);
-  });
-});
-
 app.listen(DEFAULT_PORT, HOST, () => {
   console.log(`Sample app listening on ${HOST}:${DEFAULT_PORT}!`)
 });
