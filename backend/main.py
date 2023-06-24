10db26fb5714db56b34cbf8550df2c368738f588
Avery Wagar <ajmw.subs@gmail.com>
2023-06-24 01:00:57 -0600

    Create skeleton api

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+from uuid import uuid4
 
 def route(method, path):
     def decorator(handler):
@@ -9,6 +10,36 @@ def route(method, path):
 @route('GET', '/health')
 async def health_check(request):
     return web.json_response({"status": "ok"})
+
+# Survey / Event matching
+
+@route('POST', '/survey')
+async def submit_survey(request):
+    # Returns matched event ID
+    return uuid4()
+
+@route('GET', '/event/{id}')
+async def get_event_info(request):
+    pass
+
+@route('GET', '/event')
+async def list_events(request):
+    pass
+
+# Bidding
+
+@route('POST', '/bid')
+async def create_bid(request):
+    # Returns matched event ID
+    pass
+
+@route('GET', '/bid')
+async def list_bids(request):
+    pass
+
+@route('DELETE', '/bid')
+async def delete_bid(request):
+    pass
 
 app = web.Application()
 
